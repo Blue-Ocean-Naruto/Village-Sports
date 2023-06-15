a7aed04349ffc2bf26188ebcbf21617044035171
Naru Sadakuni <narusadakuni1@gmail.com>
2023-06-14 17:16:49 -0700

    more data

--- client/sharedComponents/mockData.js
+++ client/sharedComponents/mockData.js
@@ -38,12 +38,66 @@ exports.mockData = {
           announcements: [
             {
               id: 89,
-              text: "Starting in July the Southcourt Fields will be under a landscaping remodel, so we'll be moving our regular practice location to the fields by Kinder Elementary"
+              text: "Starting in July the Southcourt Fields will be under a landscaping remodel, so we'll be moving our regular practice location to the fields by Kinder Elementary",
+              createdAt: 'Mon, 12 Jun 2023 14:00:00 GMT'
             },
             {
               id: 312,
-              text: "We're collecting group buy-ins ($10/person) for the After Scrim Dinner. Get those to the Team Captain by Friday, June 30th"
+              text: "We're collecting group buy-ins ($10/person) for the After Scrim Dinner. Get those to the Team Captain by Friday, June 30th",
+              createdAt: 'Mon, 12 Jun 2023 10:00:00 GMT'
             }
+          ]
+        },
+        {
+          id: 125,
+          name: 'Oursenal',
+          members: ['Ole', 'Mart', 'Jesus', 'Kieran'],
+          events: [
+            {
+              name: 'Game Day',
+              location: 'Cascade Stadium',
+              date: 'July 7, 2023',
+              time: '3:00PM'
+            },
+            {
+              name: 'Practice',
+              location: "Indoor spot",
+              date: 'July 10, 2023',
+              time: '6:00PM'
+            }
+          ],
+          announcements: [
+            {
+              id: 23,
+              text: "Granit and Buka are leaving us. We need another mid. Tell your friends!",
+              createdAt: 'Wed, 14 Jun 2023 20:00:00 GMT'
+            },
+          ]
+        },
+        {
+          id: 126,
+          name: 'Royal Ravens',
+          members: ['Amy', 'Alex', 'Rachel', 'Maggie', 'Karen', 'Jessica', 'Yolanda', 'Maria'],
+          events: [
+            {
+              name: 'Scrimmage vs Red Rockets',
+              location: 'Cascade Stadium',
+              date: 'July 6, 2023',
+              time: '3:00PM'
+            },
+            {
+              name: 'Girls Night',
+              location: "Karen's Chateau",
+              date: 'July 6, 2023',
+              time: '9:00PM'
+            }
+          ],
+          announcements: [
+            {
+              id: 230,
+              text: "Rachel got our league's Golden Boot award! Celebrating on July 6.",
+              createdAt: 'Wed, 14 Jun 2023 20:00:00 GMT'
+            },
           ]
         }
       ]
@@ -86,8 +140,50 @@ exports.mockData = {
           announcements: [
             {
               id: 8,
-              text: "Attention all members of the Frost King hockey team! We are thrilled to announce our upcoming game against the Icebreakers this Saturday, June 17th at 7PM. Our team has been training hard, refining our skills, and strategizing to come out strong and secure the victory. We're ready to showcase our teamwork, determination, and love for the game."
+              text: "Attention all members of the Frost King hockey team! We are thrilled to announce our upcoming game against the Icebreakers this Saturday, June 17th at 7PM. Our team has been training hard, refining our skills, and strategizing to come out strong and secure the victory. We're ready to showcase our teamwork, determination, and love for the game.",
+              createdAt: 'Tue, 13 Jun 2023 20:00:00 GMT'
             }
+          ]
+        },
+        {
+          id: 1355,
+          name: 'Icebreakers',
+          members: ['Van', 'Ian', 'Mack', 'Michael', 'Matt', 'Matthew', 'Matias'],
+          events: [
+            {
+              name: 'Game vs Frost Kings',
+              location: 'Glacier Arena',
+              date: 'June 17, 2023',
+              time: '7:00PM'
+            },
+            {
+              name: 'Freeskate',
+              location: 'Blizzard Bay Rink',
+              date: 'June 20, 2023',
+              time: '3:00PM'
+            },
+          ],
+          announcements: [
+            {
+              id: 10,
+              text: "Michael's collar bone is broken from last week's game. Collecting funds to help with his medical bills.",
+              createdAt: 'Wed, 14 Jun 2023 20:00:00 GMT'
+            }
+          ]
+        },
+        {
+          id: 1356,
+          name: 'Absolute Zero',
+          members: ['Anders', 'Bose', 'Albert', 'Maxwell', 'Ludwig'],
+          events: [
+            {
+              name: 'Experimental Drills',
+              location: 'Galloway',
+              date: 'June 19, 2023',
+              time: '10:00AM'
+            },
+          ],
+          announcements: [
           ]
         }
       ]
@@ -130,7 +226,34 @@ exports.mockData = {
           announcements: [
             {
               id: 765,
-              text: "Due to popular demand, we are pleased to announce the formation of our brand-new TFT team, Fortune's Fury. Stay tuned for further updates"
+              text: "Due to popular demand, we are pleased to announce the formation of our brand-new TFT team, Fortune's Fury. Stay tuned for further updates",
+              createdAt: 'Tue, 13 Jun 2023 10:00:00 GMT'
+            }
+          ]
+        },
+        {
+          id: 12,
+          name: 'Toxic',
+          members: ['Dota', 'Fox', 'Chen', 'Mag', 'Sandy', 'Jamal'],
+          events: [
+            {
+              name: 'Team Bonding',
+              location: "The Fox Box",
+              date: 'June 22, 2023',
+              time: '8:30PM'
+            },
+            {
+              name: 'Chain Queue',
+              location: "Summoner's Rift",
+              date: 'July 24, 2023',
+              time: '7:00AM'
+            }
+          ],
+          announcements: [
+            {
+              id: 76,
+              text: "BYO for Team Bonding",
+              createdAt: 'Wed, 14 Jun 2023 20:00:00 GMT'
             }
           ]
         }
@@ -180,15 +303,44 @@ exports.mockData = {
           announcements: [
             {
               id: 17,
-              text: "Attention Volley Vipers! We are excited to announce the launch of our new website, where you can find all the latest updates, match schedules, standings, and more. Visit the URL and explore the features to stay connected with our tennis community."
+              text: "Attention Volley Vipers! We are excited to announce the launch of our new website, where you can find all the latest updates, match schedules, standings, and more. Visit the URL and explore the features to stay connected with our tennis community.",
+              createdAt: 'Sat, 10 Jun 2023 12:00:00 GMT'
             },
             {
               id: 390,
-              text: "Should we start a tournament bracket for the summer? Let us know in the poll"
+              text: "Should we start a tournament bracket for the summer? Let us know in the poll",
+              createdAt: 'Tue, 13 Jun 2023 10:00:00 GMT'
             },
             {
               id: 555,
-              text: "Vipers, make sure you are checking the locations for drill practice because we are rotating with other teams in the league"
+              text: "Vipers, make sure you are checking the locations for drill practice because we are rotating with other teams in the league",
+              createdAt: 'Mon, 12 Jun 2023 22:00:00 GMT'
+            }
+          ]
+        },
+        {
+          id: 998,
+          name: 'Advantage',
+          members: ['Benji', 'Tyrone', 'Dan', 'Michelle'],
+          events: [
+            {
+              name: '1v1',
+              location: 'Clay Grounds',
+              date: 'June 22, 2023',
+              time: '12:30PM'
+            },
+            {
+              name: 'Match Day',
+              location: 'Grand Slam Gardens',
+              date: 'June 23, 2023',
+              time: '2:00PM'
+            },
+          ],
+          announcements: [
+            {
+              id: 11111,
+              text: "Before practice on June 22: Benji is going to teach us how to get in our opponent's head. Don't be late!",
+              createdAt: 'Wed, 14 Jun 2023 12:00:00 GMT'
             }
           ]
         }
@@ -226,12 +378,55 @@ exports.mockData = {
           announcements: [
             {
               id: 42,
-              text: "Tryouts for team quarterback in the August lineup will start NEXT WEEK. Let's go, Rhinos!"
+              text: "Tryouts for team quarterback in the August lineup will start NEXT WEEK. Let's go, Rhinos!",
+              createdAt: 'Sat, 10 Jun 2023 12:00:00 GMT'
             },
             {
               id: 962,
-              text: "Though not official, Coach is in talks with Jeanine from the varsity cheer squad to have them cheer for us on our next big game 👀"
+              text: "Though not official, Coach is in talks with Jeanine from the varsity cheer squad to have them cheer for us on our next big game 👀",
+              createdAt: 'Sun, 11 Jun 2023 22:00:00 GMT'
             }
+          ]
+        },
+        {
+          id: 99871,
+          name: 'Concussion',
+          members: ['Sahid', 'Bob', 'Chad', 'Chadius'],
+          events: [
+            {
+              name: 'Practice',
+              location: 'Golden Gate Park',
+              date: 'June 22, 2023',
+              time: '12:30PM'
+            },
+            {
+              name: 'Lifting',
+              location: "Gold's Gym",
+              date: 'June 24, 2023',
+              time: '8:00PM'
+            }
+          ],
+          announcements: [
+            {
+              id: 321,
+              text: "Anyone heard from Sahid?",
+              createdAt: 'Mon, 12 Jun 2023 10:00:00 GMT'
+            },
+          ]
+        },
+        {
+          id: 9171,
+          name: 'Shortest Yard',
+          members: ['Chadd', 'Paul', 'Nate', 'Chris', 'Duane', 'Cap', 'Tony', 'Tucker'],
+          events: [
+            {
+              name: 'Practice',
+              location: 'Golden Gate Park',
+              date: 'June 22, 2023',
+              time: '2:30PM'
+            },
+          ],
+          announcements: [
           ]
         }
       ]
@@ -286,8 +481,50 @@ exports.mockData = {
           announcements: [
             {
               id: 33382,
-              text: "Caleb, you left your water bottle in the locker room. It's in the Lost and Found at the front desk of Alley-Oop Arena"
+              text: "Caleb, you left your water bottle in the locker room. It's in the Lost and Found at the front desk of Alley-Oop Arena",
+              createdAt: 'Wed, 14 Jun 2023 20:00:00 GMT'
             }
+          ]
+        },
+        {
+          id: 20,
+          name: 'Dr. Jay and friends',
+          members: ['Jay', 'MJ', 'Erving', 'Colby', 'Shaqira', 'Spud'],
+          events: [
+            {
+              name: 'Dunk Contest',
+              location: 'Alley-Oop Arena',
+              date: 'June 28, 2023',
+              time: '2:30PM'
+            },
+            {
+              name: 'Training Session',
+              location: "Fastbreak Fitness Center",
+              date: 'July 1, 2023',
+              time: '10:00AM'
+            },
+          ],
+          announcements: [
+            {
+              id: 3382,
+              text: "After last Monday's no-show, Jay wants everyone to share their location on game days so we can keep tabs on each other.",
+              createdAt: 'Tue, 13 Jun 2023 20:00:00 GMT'
+            }
+          ]
+        },
+        {
+          id: 220,
+          name: 'Posterize',
+          members: ['Ron', 'Meta', 'Patrick', 'Larry', 'Dennis'],
+          events: [
+            {
+              name: 'Dunk Contest',
+              location: 'Alley-Oop Arena',
+              date: 'June 28, 2023',
+              time: '2:30PM'
+            },
+          ],
+          announcements: [
           ]
         }
       ]
