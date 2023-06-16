bd75808c6e3e935d6d1d208bc521895ec6dcc6bd
Gaoyuan Zhang <zgy25483387@gmail.com>
2023-06-16 10:46:27 -0700

    unparsed data

--- client/home/TempWatchList.jsx
+++ client/home/TempWatchList.jsx
@@ -10,6 +10,13 @@ import UsernameContext from '../sharedComponents/UsernameContext.jsx';
 export default function TempWatchList({ navigation }) {
   const { leagues } = useContext(UsernameContext);
   const wishList = leagues.slice(leagues.length - 2, leagues.length);
+
+  wishList.forEach((league) => {
+    const parsedTeams = league.teams.map((team) => {
+      return JSON.parse(team);
+    });
+    league.teams = parsedTeams;
+  });
   return (
     <LinearView>
       <LeaguesSaved navigation={navigation} wishList={wishList} />
